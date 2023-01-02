c403e3f31f245d1ab036a29d434ea5e79ba90dbc
Mia <mia@trustfactory.bz>
2023-01-02 22:25:17 +0100

    Added getting hashed file name for url

--- src/pogge.rs
+++ src/pogge.rs
@@ -3,6 +3,8 @@ mod fetche;
 
 use error_chain::error_chain;
 use self::fetche::loads_words_array_from_url;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 // logic for the app itself, like generating things etc.
 error_chain! {
@@ -38,6 +40,15 @@ impl Nutter {
         // the whole URL load should probably happen in a separate method, etc. etc.
         // it probably should just load data from urls and save them if needed
         for url in library_urls {
+            // TODO some thing here that first checks the url file, if it doesnt exists, crete it
+            // and fetch words, put them in, etc.
+            let hashed_url = Nutter::hash_string(url.clone());
+            let filename = format!("{}.txt", hashed_url);
+
+            // TODO pass the filename to filere.rs to check if file exists. if it does, go over, if
+            // doesn't, load words and store them in the file, one per line
+
+            println!("Hashed string: {}", hashed_url);
             let entities: Vec<String> = loads_words_array_from_url(url);
 
             for entity in entities {
@@ -51,5 +62,13 @@ impl Nutter {
         // after the above, scan all files and load stuff and start building the whole graph
 
         return self;
+    }
+
+    fn hash_string(string: String) -> u64 {
+        let mut hasher = DefaultHasher::new();
+
+        string.hash(&mut hasher);
+
+        return hasher.finish();
     }
 }
